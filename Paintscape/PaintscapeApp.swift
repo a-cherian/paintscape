f8574be78b1218fd211a991ce3586d6ddbdaf316
a-cherian <cherian@calpoly.edu>
2023-08-15 21:08:21 -0700

    Initial Commit

---
+++ Paintscape/PaintscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintscapeApp.swift
+//  Paintscape
+//
+//  Created by AC on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct PaintscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
